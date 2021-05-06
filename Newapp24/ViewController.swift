7bce5c18e9bf3ea0ca451b2845ace09b6de1421a
UA-Holodeck <rishavk@email.arizona.edu>
2021-05-06 13:52:44 -0700

    IC

--- Newapp24/ViewController.swift
+++ Newapp24/ViewController.swift
@@ -6,14 +6,70 @@
 //
 
 import UIKit
+import CocoaMQTT
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController,CocoaMQTTDelegate {
+   
+    @IBOutlet var imageView: UIImageView!
+    func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
+        mqtt.subscribe("tag/pics")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
+        
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
+        
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16) {
+        print((message.string!));
+        imageView.image = UIImage(named:message.string!)
+
+        
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopics success: NSDictionary, failed: [String]) {
+        
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopics topics: [String]) {
+        
+    }
+    
+    func mqttDidPing(_ mqtt: CocoaMQTT) {
+        
+    }
+    
+    func mqttDidReceivePong(_ mqtt: CocoaMQTT) {
+        
+    }
+    
+    func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
+        
+    }
+ 
 
     override func viewDidLoad() {
+        
+
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let clientID = "CocoaMQTT-" + String(ProcessInfo().processIdentifier)
+        let websocket = CocoaMQTTWebSocket(uri: "/mqtt")
+        var mqtt = CocoaMQTT(clientID: clientID, host: "192.168.1.4", port: 1884, socket: websocket)
+        
+
+              mqtt.keepAlive = 60
+              mqtt.delegate = self
+          
+        print(mqtt.connect())
+       
+       
     }
-
-
+  
 }
+
 
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+ pod 'CocoaMQTT/WebSockets', '1.3.0-rc.1'
+
+target 'Newapp24' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Newapp24
+
+  target 'Newapp24Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Newapp24UITests' do
+    # Pods for testing
+  end
+
+end
